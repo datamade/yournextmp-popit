30e7868b98bb3fa094f7c2a0b20a14b884a3592c
Mark Longair <mhl@pobox.com>
2015-04-09 16:43:48 +0100

    fixup! Initial official_documents app

--- official_documents/migrations/0003_add_group.py
+++ official_documents/migrations/0003_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import migrations
 
 from auth_helpers.migrations import (
@@ -13,6 +14,7 @@ from official_documents.models import DOCUMENT_UPLOADERS_GROUP_NAME
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('official_documents', '0002_officialdocument_document_type'),
     ]
 
@@ -25,12 +27,4 @@ class Migration(migrations.Migration):
                 DOCUMENT_UPLOADERS_GROUP_NAME
             )
         ),
-        migrations.RunPython(
-            get_migration_group_create(
-                DOCUMENT_UPLOADERS_GROUP_NAME, ['change_officialdocument']
-            ),
-            get_migration_group_delete(
-                DOCUMENT_UPLOADERS_GROUP_NAME
-            )
-        )
     ]
