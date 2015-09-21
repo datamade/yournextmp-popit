beb7d86c234084e31c8c37c132d31fff36001add
Eric van Zanten <eric.vanzanten@gmail.com>
2015-09-21 17:11:20 -0500

    St Paul stubbed out

--- elections/st_paul_municipal_2015/management/commands/st_paul_load_candidates.py
+++ elections/st_paul_municipal_2015/management/commands/st_paul_load_candidates.py
@@ -111,6 +111,7 @@ class Command(BaseCommand):
             else:
                 party_id = 'party:401'
 
+
             party_name = PARTY_DATA.party_id_to_name[party_id]
 
             person.party_memberships = {
